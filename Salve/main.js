49679b73e905ecaf99d11d820b24353846e77024
Lucas Oliveira de Borba <56658658+oliverborba@users.noreply.github.com>
2025-12-08 23:04:28 -0300

    Delete Salve directory

--- Salve/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const yearElement = document.getElementById('year');
-    if (yearElement) {
-        yearElement.textContent = new Date().getFullYear();
-    }
-});
